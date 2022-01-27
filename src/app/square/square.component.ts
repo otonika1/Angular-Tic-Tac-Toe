3ca33a290d1d65e01b21c95a0130c125510e222d
otonika10 <77378386+otonika10@users.noreply.github.com>
2022-01-27 21:17:46 +0400

    feat(additional changes)

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -2,23 +2,24 @@ import { style } from '@angular/animations';
 import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
+  
   selector: 'app-square',
   template: `
-  <button nbButton *ngIf="!value"
-  style="height: 200px;
-   width: 200px;
-   font-size:80px;
-   ">{{ value }}</button>
-     <button  nbButton hero status="success" *ngIf="value== 'X' "
-  style="height: 200px;
-   width: 200px;
-   font-size:80px;
-   ">{{ value }}</button>
-   <button  nbButton hero status="success" *ngIf="value== 'O' "
-  style="height: 200px;
-   width: 200px;
-   font-size:80px;
-   ">{{ value }}</button>
+  <style>
+    button {
+      height: 200px;
+      width: 200px;
+    }
+    @media screen and (max-width: 800px) {
+      button {
+      height: 80px;
+      width: 80px;
+      }
+    }
+  </style>
+  <button style="font-size:80px"  nbButton *ngIf="!value">{{ value }}</button>
+     <button style="font-size:80px" nbButton hero status="success" *ngIf="value== 'X' ">{{ value }}</button>
+   <button style="font-size:80px" nbButton hero status="success" *ngIf="value== 'O' ">{{ value }}</button>
   `,
   styles:[]
 })
